573af1a467e4c71c6dab878281f0a02fd8a3a699
최현호 <x7788@naver.com>
2021-09-24 19:37:23 +0900

    가로유형메뉴-2

---
+++ scripts/row2.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.nav > ul > li').mouseover(function(){
+        $('.nav > ul > li > ul').stop().slideDown(200)
+    });
+
+    $('.nav > ul > li').mouseout(function(){
+        $('.nav > ul > li > ul').stop().slideUp(200)
+    });
+});
